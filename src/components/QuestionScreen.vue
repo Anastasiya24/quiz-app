d79e40e66c86bb974838aa14964361d2bd16127a
A.Shuhouskaya <A.Shuhouskaya@itechart-group.com>
2019-04-15 19:32:11 +0300

    Added QuestionScreen

---
+++ src/components/QuestionScreen.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="question-screen">
+        <h1 class="question-screen__question">
+            Contrary to popular belief, Lorem Ipsum is not simply random text?
+        </h1>  
+        <div class="question-screen__slider">
+            <div>
+                <button>Back</button>
+                <p class="question-screen__quiz-switch">Back</p>
+            </div>
+            <p class="question-screen__quiz-note">
+                Select all that apply
+            </p>
+            <div>
+                <button>Skip</button>
+                <p class="question-screen__quiz-switch">Skip</p>
+            </div>
+        </div>
+        <div class="question-screen__answers">
+            <button>0-3 Years</button>
+            <button>4-9 Years</button>
+            <button>10+ Years</button>
+        </div>
+        <p class="question-screen__quiz-counter">
+            1/6 complete
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'QuestionScreen'
+}
+</script>
+
+<style scoped>
+    .question-screen {
+        background-color: black;
+        color: white;
+        padding: 50px 70px 20px;
+    }
+    .question-screen__question {
+        font-weight: 550;
+        text-transform: uppercase;
+    }
+    .question-screen__quiz-note,
+    .question-screen__quiz-switch,
+    .question-screen__quiz-counter {
+        font-size: 13px;
+        font-style: italic;
+    }
+    .question-screen__quiz-counter {
+        text-align: right;
+    }
+    .question-screen__slider{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
--- src/components/WelcomeScreen.vue
+++ src/components/WelcomeScreen.vue
@@ -1,21 +1,21 @@
 <template>
-<div class="main-screen">
-    <h1 class="main-screen__header">
-        Welcome Screen. Lorem Ipsum is simply dummy text of the printing and typesetting industry!
-    </h1>  
-    <p class="main-screen__quiz-description">
-        There are many variations of passages of Lorem Ipsum available, 
-        but the majority have suffered alteration in some form, by injected humour, 
-        or randomised words which don't look even slightly believable. 
-        If you are going to use a passage of Lorem Ipsum, you need to be sure there 
-        isn't anything embarrassing hidden in the middle of text. 
-    </p>  
-    <button>Start</button>
-    <p class="main-screen__quiz-note">
-        It is a long established fact that a reader will be distracted by the readable 
-        content of a page when looking at its layout. 
-    </p>
-</div>
+    <div class="main-screen">
+        <h1 class="main-screen__header">
+            Welcome Screen. Lorem Ipsum is simply dummy text of the printing and typesetting industry!
+        </h1>  
+        <p class="main-screen__quiz-description">
+            There are many variations of passages of Lorem Ipsum available, 
+            but the majority have suffered alteration in some form, by injected humour, 
+            or randomised words which don't look even slightly believable. 
+            If you are going to use a passage of Lorem Ipsum, you need to be sure there 
+            isn't anything embarrassing hidden in the middle of text. 
+        </p>  
+        <button class="main-screen__button">Start</button>
+        <p class="main-screen__quiz-note">
+            It is a long established fact that a reader will be distracted by the readable 
+            content of a page when looking at its layout. 
+        </p>
+    </div>
 </template>
 
 
@@ -26,19 +26,28 @@ export default {
 </script>
 
 <style scoped>
-.main-screen {
-    background-color: black
-}
-.main-screen__header,
-.main-screen__quiz-description {
-    color: white;
-    text-transform: uppercase;
-}
-.main-screen__header {
-    font-weight: 900;
-}
-.main-screen__quiz-note {
-    color: yellow;
-    font-style: italic;
-}
+    .main-screen {
+        background-color: black;
+        padding: 50px 70px 20px;
+    }
+    
+    .main-screen__header,
+    .main-screen__quiz-description {
+        color: white;
+        text-transform: uppercase;
+    }
+
+    .main-screen__button {
+        margin: 30px;
+    }
+
+    .main-screen__header {
+        font-weight: 550;
+        margin-bottom: 100px;
+    }
+
+    .main-screen__quiz-note {
+        color: yellow;
+        font-style: italic;
+    }
 </style>
